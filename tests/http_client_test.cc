6bd35f3772fa378e1b0abce3eef360d5a0b65651
Dima <dmitry.korolev@gmail.com>
2014-12-09 14:45:04 -1000

    HTTP Client Wrapper.

--- tests/http_client_test.cc
+++ tests/http_client_test.cc
@@ -1,3 +1,5 @@
+// TODO(dkorolev): Add a 404 test for downloading into file.
+
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -26,7 +28,7 @@ using std::string;
 using std::thread;
 using std::to_string;
 
-using aloha::HttpClient;
+using aloha::HTTPClientPlatformWrapper;
 
 using bricks::ScopeGuard;
 using bricks::MakeScopeGuard;
@@ -40,6 +42,165 @@ using bricks::net::HTTPResponseCode;
 
 DEFINE_int32(port, 8080, "Local port to use for the test HTTP server.");
 DEFINE_string(test_tmpdir, ".", "Local path for the test to create temporary files in.");
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO(dkorolev): Merge exception types into bricks/net.
+struct HTTPClientException : std::exception {};
+
+struct HTTPRequestGET {
+  string url;
+  string custom_user_agent;
+  explicit HTTPRequestGET(const string& url) : url(url) {
+  }
+  HTTPRequestGET& SetUserAgent(const string& ua) {
+    custom_user_agent = ua;
+    return *this;
+  }
+  void PopulateClient(HTTPClientPlatformWrapper& client) const {
+    client.set_url_requested(url);
+    if (!custom_user_agent.empty()) {
+      client.set_user_agent(custom_user_agent);
+    }
+  }
+};
+
+struct HTTPRequestPOST {
+  string url;
+  string body;
+  string content_type;
+  explicit HTTPRequestPOST(const string& url, const string& body, const string& content_type)
+      : url(url), body(body), content_type(content_type) {
+  }
+  void PopulateClient(HTTPClientPlatformWrapper& client) const {
+    client.set_url_requested(url);
+    client.set_post_body(body, content_type);
+  }
+};
+
+struct HTTPRequestPOSTFromFile {
+  string url;
+  string file_name;
+  string content_type;
+  explicit HTTPRequestPOSTFromFile(const string& url, const string& file_name, const string& content_type)
+      : url(url), file_name(file_name), content_type(content_type) {
+  }
+  void PopulateClient(HTTPClientPlatformWrapper& client) const {
+    client.set_url_requested(url);
+    client.set_post_file(file_name, content_type);
+  }
+};
+
+HTTPRequestGET GET(const string& url) {
+  return HTTPRequestGET(url);
+}
+
+HTTPRequestPOST POST(const string& url, const string& body, const string& content_type) {
+  return HTTPRequestPOST(url, body, content_type);
+}
+
+HTTPRequestPOSTFromFile POSTFromFile(const string& url, const string& file_name, const string& content_type) {
+  return HTTPRequestPOSTFromFile(url, file_name, content_type);
+}
+
+struct HTTPResponse {
+  // Request.
+  string url;
+
+  // Response code.
+  // Response body can be either `string body` or `string body_file_name`,
+  // defined in `struct DownoadData` and `struct DownloadFile` respectively.
+  int code;
+
+  // Redirect metadata.
+  // TODO(dkorolev): Add a test when the redirects end up pointing to the same original URL,
+  //                 so that (url_after_redirects == url && was_redirected == true).
+  string url_after_redirects;
+  bool was_redirected;
+
+  template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
+  void Populate(const T_REQUEST_PARAMS& request_params,
+                const T_RESPONSE_PARAMS& response_params,
+                const HTTPClientPlatformWrapper& response) {
+    assert(request_params.url == response.url_requested());
+    url = request_params.url;
+    code = response.error_code();
+    url_after_redirects = response.url_received();
+    was_redirected = response.was_redirected();
+  }
+};
+
+struct HTTPResponseWithBuffer : HTTPResponse {
+  // Returned HTTP body, saved as an in-memory buffer, stored in std::string.
+  string body;
+
+  template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
+  void Populate(const T_REQUEST_PARAMS& request_params,
+                const T_RESPONSE_PARAMS& response_params,
+                const HTTPClientPlatformWrapper& response) {
+    HTTPResponse::Populate(request_params, response_params, response);
+    body = response.server_response();
+  }
+};
+
+struct HTTPResponseWithResultingFileName : HTTPResponse {
+  // The file name into which the returned HTTP body has been saved.
+  string body_file_name;
+
+  template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
+  void Populate(const T_REQUEST_PARAMS& request_params,
+                const T_RESPONSE_PARAMS& response_params,
+                const HTTPClientPlatformWrapper& response) {
+    HTTPResponse::Populate(request_params, response_params, response);
+    body_file_name = response_params.file_name;
+  }
+};
+
+struct KeepResponseInMemory {
+  void PopulateClient(HTTPClientPlatformWrapper& client) const {
+  }
+};
+
+struct SaveResponseToFile {
+  string file_name;
+  explicit SaveResponseToFile(const string& file_name) : file_name(file_name) {
+  }
+  void PopulateClient(HTTPClientPlatformWrapper& client) const {
+    client.set_received_file(file_name);
+  }
+};
+
+template <typename T>
+struct ResponseTypeFromRequestType {};
+
+template <>
+struct ResponseTypeFromRequestType<KeepResponseInMemory> {
+  typedef HTTPResponseWithBuffer T_RESPONSE_TYPE;
+};
+
+template <>
+struct ResponseTypeFromRequestType<SaveResponseToFile> {
+  typedef HTTPResponseWithResultingFileName T_RESPONSE_TYPE;
+};
+
+template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS = KeepResponseInMemory>
+inline typename ResponseTypeFromRequestType<T_RESPONSE_PARAMS>::T_RESPONSE_TYPE HTTP(
+    const T_REQUEST_PARAMS& request_params,
+    const T_RESPONSE_PARAMS& response_params = T_RESPONSE_PARAMS()) {
+  HTTPClientPlatformWrapper client;
+  request_params.PopulateClient(client);
+  response_params.PopulateClient(client);
+  if (!client.RunHTTPRequest()) {
+    throw HTTPClientException();
+  }
+  typename ResponseTypeFromRequestType<T_RESPONSE_PARAMS>::T_RESPONSE_TYPE result;
+  result.Populate(request_params, response_params, client);
+  return result;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 
 class UseHTTPBinTestServer {
  public:
@@ -150,165 +311,151 @@ static auto ScopedFileCleanup = [](const string& file_name) {
 };
 
 template <typename T>
-class HTTPClient : public ::testing::Test {};
+class HTTPClientTemplatedTest : public ::testing::Test {};
 
 typedef ::testing::Types<UseLocalHTTPTestServer, UseHTTPBinTestServer> HTTPClientTestTypeList;
-TYPED_TEST_CASE(HTTPClient, HTTPClientTestTypeList);
+TYPED_TEST_CASE(HTTPClientTemplatedTest, HTTPClientTestTypeList);
 
-TYPED_TEST(HTTPClient, GetIntoMemory) {
+TYPED_TEST(HTTPClientTemplatedTest, GetToBuffer) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/drip?numbytes=7";
-  HttpClient client(url);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_EQ(200, client.error_code());
-  EXPECT_EQ("*******", client.server_response());
-  EXPECT_EQ(url, client.url_received());
-  EXPECT_FALSE(client.was_redirected());
+  auto response = HTTP(GET(url));
+  EXPECT_EQ(200, response.code);
+  EXPECT_EQ("*******", response.body);
+  EXPECT_EQ(url, response.url);
+  EXPECT_EQ(url, response.url_after_redirects);
+  EXPECT_FALSE(response.was_redirected);
 }
 
-TYPED_TEST(HTTPClient, GetIntoFile) {
+TYPED_TEST(HTTPClientTemplatedTest, GetToFile) {
   const string file_name = FLAGS_test_tmpdir + "/some_test_file_for_http_get";
   const auto test_file_scope = ScopedFileCleanup(file_name);
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/drip?numbytes=5";
-  HttpClient client(url);
-  client.set_received_file(file_name);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_TRUE(client.server_response().empty());
-  EXPECT_EQ("*****", ReadFileAsString(file_name));
+  auto response = HTTP(GET(url), SaveResponseToFile(file_name));
+  EXPECT_EQ(200, response.code);
+  EXPECT_EQ(file_name, response.body_file_name);
+  EXPECT_EQ(url, response.url);
+  EXPECT_EQ(url, response.url_after_redirects);
+  EXPECT_FALSE(response.was_redirected);
+  EXPECT_EQ("*****", ReadFileAsString(response.body_file_name));
 }
 
-TYPED_TEST(HTTPClient, PostFromMemoryIntoMemory) {
+TYPED_TEST(HTTPClientTemplatedTest, PostFromBufferToBuffer) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/post";
-  string const post_body = "Hello, World!";
-  HttpClient client(url);
-  client.set_post_body(post_body, "application/octet-stream");
-  ASSERT_TRUE(client.Connect());
-  EXPECT_NE(string::npos, client.server_response().find("\"data\": \"" + post_body + "\""))
-      << client.server_response();
+  const string post_body = "Hello, World!";
+  auto response = HTTP(POST(url, post_body, "application/octet-stream"));
+  EXPECT_NE(string::npos, response.body.find("\"data\": \"" + post_body + "\"")) << response.body;
 }
 
-TYPED_TEST(HTTPClient, PostFromInvalidFile) {
+TYPED_TEST(HTTPClientTemplatedTest, PostFromInvalidFile) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/post";
-  HttpClient client(url);
-  client.set_post_file("this_file_should_not_exist", "text/plain");
-  ASSERT_FALSE(client.Connect());
-  EXPECT_NE(200, client.error_code());
-
+  const string non_existent_file_name = FLAGS_test_tmpdir + "/non_existent_file";
+  const auto test_file_scope = ScopedFileCleanup(non_existent_file_name);
+  ASSERT_THROW(HTTP(POSTFromFile(url, non_existent_file_name, "text/plain")), HTTPClientException);
   // Still do one request since local HTTP server is waiting for it.
-  ASSERT_TRUE(HttpClient(TypeParam::BaseURL() + "/get").Connect());
+  EXPECT_EQ(200, HTTP(GET(TypeParam::BaseURL() + "/get")).code);
 }
 
-TYPED_TEST(HTTPClient, PostFromFileIntoMemory) {
+TYPED_TEST(HTTPClientTemplatedTest, PostFromFileToBuffer) {
   const string file_name = FLAGS_test_tmpdir + "/some_input_test_file_for_http_post";
   const auto test_file_scope = ScopedFileCleanup(file_name);
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/post";
-  // Use it as a file name and as a test contents string.
   WriteStringToFile(file_name, file_name);
-  HttpClient client(url);
-  client.set_post_file(file_name, "text/plain");
-  ASSERT_TRUE(client.Connect());
-  EXPECT_NE(string::npos, client.server_response().find(file_name));
+  auto response = HTTP(POSTFromFile(url, file_name, "application/octet-stream"));
+  EXPECT_EQ(200, response.code);
+  EXPECT_NE(string::npos, response.body.find(file_name));
 }
 
-TYPED_TEST(HTTPClient, PostFromMemoryIntoFile) {
+TYPED_TEST(HTTPClientTemplatedTest, PostFromBufferToFile) {
   const string file_name = FLAGS_test_tmpdir + "/some_output_test_file_for_http_post";
   const auto test_file_scope = ScopedFileCleanup(file_name);
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/post";
-  HttpClient client(url);
-  client.set_received_file(file_name).set_post_body("TEST BODY", "text/plain");
-  ASSERT_TRUE(client.Connect());
-  EXPECT_TRUE(client.server_response().empty());
-  EXPECT_NE(string::npos, ReadFileAsString(file_name).find("TEST BODY"));
+  auto response = HTTP(POST(url, "TEST BODY", "text/plain"), SaveResponseToFile(file_name));
+  EXPECT_EQ(200, response.code);
+  EXPECT_NE(string::npos, ReadFileAsString(response.body_file_name).find("TEST BODY"));
 }
 
-TYPED_TEST(HTTPClient, PostFromFileIntoFile) {
-  const string input_file_name = FLAGS_test_tmpdir + "/some_complex_input_test_file_for_http_post";
-  const string output_file_name = FLAGS_test_tmpdir + "/some_complex_output_test_file_for_http_post";
-  const auto input_file_scope = ScopedFileCleanup(input_file_name);
-  const auto output_file_scope = ScopedFileCleanup(output_file_name);
+TYPED_TEST(HTTPClientTemplatedTest, PostFromFileToFile) {
+  const string request_file_name = FLAGS_test_tmpdir + "/some_complex_request_test_file_for_http_post";
+  const string response_file_name = FLAGS_test_tmpdir + "/some_complex_response_test_file_for_http_post";
+  const auto input_file_scope = ScopedFileCleanup(request_file_name);
+  const auto output_file_scope = ScopedFileCleanup(response_file_name);
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/post";
   const string post_body = "Aloha, this text should pass from one file to another. Mahalo!";
-  WriteStringToFile(input_file_name, post_body);
-  HttpClient client(url);
-  client.set_post_file(input_file_name, "text/plain").set_received_file(output_file_name);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_TRUE(client.server_response().empty());
+  WriteStringToFile(request_file_name, post_body);
+  auto response =
+      HTTP(POSTFromFile(url, request_file_name, "text/plain"), SaveResponseToFile(response_file_name));
+  EXPECT_EQ(200, response.code);
   {
-    const string received_data = ReadFileAsString(output_file_name);
+    const string received_data = ReadFileAsString(response.body_file_name);
     EXPECT_NE(string::npos, received_data.find(post_body)) << received_data << endl << post_body;
   }
 }
 
-TYPED_TEST(HTTPClient, ErrorCodes) {
+TYPED_TEST(HTTPClientTemplatedTest, ErrorCodes) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/status/403";
-  HttpClient client(url);
-  ASSERT_FALSE(client.Connect());
-  EXPECT_EQ(403, client.error_code());
+  EXPECT_EQ(403, HTTP(GET(url)).code);
 }
 
-TYPED_TEST(HTTPClient, Https) {
+TYPED_TEST(HTTPClientTemplatedTest, Https) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/get?Aloha=Mahalo";
-  HttpClient client(url);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_EQ(200, client.error_code());
-  EXPECT_FALSE(client.was_redirected());
-  EXPECT_NE(string::npos, client.server_response().find("\"Aloha\": \"Mahalo\""));
+  auto response = HTTP(GET(url));
+  EXPECT_EQ(url, response.url);
+  EXPECT_EQ(200, response.code);
+  EXPECT_FALSE(response.was_redirected);
+  EXPECT_NE(string::npos, response.body.find("\"Aloha\": \"Mahalo\""));
 }
 
-TYPED_TEST(HTTPClient, HttpRedirect302) {
+TYPED_TEST(HTTPClientTemplatedTest, HttpRedirect302) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/redirect-to?url=/get";
-  HttpClient client(url);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_EQ(TypeParam::BaseURL() + "/get", client.url_received());
-  EXPECT_TRUE(client.was_redirected());
+  auto response = HTTP(GET(url));
+  EXPECT_EQ(url, response.url);
+  EXPECT_EQ(200, response.code);
+  EXPECT_EQ(TypeParam::BaseURL() + "/get", response.url_after_redirects);
+  EXPECT_TRUE(response.was_redirected);
 }
 
-TYPED_TEST(HTTPClient, UserAgent) {
+TYPED_TEST(HTTPClientTemplatedTest, UserAgent) {
   const auto server_scope = TypeParam::SpawnServer();
   const string url = TypeParam::BaseURL() + "/user-agent";
-  HttpClient client(url);
   const string custom_user_agent = "Aloha User Agent";
-  client.set_user_agent(custom_user_agent);
-  ASSERT_TRUE(client.Connect());
-  EXPECT_NE(string::npos, client.server_response().find(custom_user_agent));
+  auto response = HTTP(GET(url).SetUserAgent(custom_user_agent));
+  EXPECT_EQ(url, response.url);
+  EXPECT_EQ(200, response.code);
+  EXPECT_NE(string::npos, response.body.find(custom_user_agent));
 }
 
 // TODO(dkorolev): Get rid of the tests involving external URLs.
-TYPED_TEST(HTTPClient, HttpRedirect301) {
+TYPED_TEST(HTTPClientTemplatedTest, HttpRedirect301) {
   if (TypeParam::SupportsExternalURLs()) {
-    const auto server_scope = TypeParam::SpawnServer();
-    HttpClient client("http://github.com");
-    ASSERT_TRUE(client.Connect());
-    EXPECT_NE(client.url_requested(), client.url_received());
-    EXPECT_EQ("https://github.com/", client.url_received());
-    EXPECT_TRUE(client.was_redirected());
+    auto response = HTTP(GET("http://github.com"));
+    EXPECT_EQ(200, response.code);
+    EXPECT_TRUE(response.was_redirected);
+    EXPECT_EQ("https://github.com/", response.url_after_redirects);
   }
 }
 
-TYPED_TEST(HTTPClient, HttpRedirect307) {
+TYPED_TEST(HTTPClientTemplatedTest, HttpRedirect307) {
   if (TypeParam::SupportsExternalURLs()) {
-    const auto server_scope = TypeParam::SpawnServer();
-    HttpClient client("http://msn.com");
-    ASSERT_TRUE(client.Connect());
-    EXPECT_EQ("http://www.msn.com/", client.url_received());
-    EXPECT_TRUE(client.was_redirected());
+    auto response = HTTP(GET("http://msn.com"));
+    EXPECT_EQ(200, response.code);
+    EXPECT_TRUE(response.was_redirected);
+    EXPECT_EQ("http://www.msn.com/", response.url_after_redirects);
   }
 }
 
-TYPED_TEST(HTTPClient, InvalidUrl) {
+TYPED_TEST(HTTPClientTemplatedTest, InvalidUrl) {
   if (TypeParam::SupportsExternalURLs()) {
-    const auto server_scope = TypeParam::SpawnServer();
-    HttpClient client("http://very.bad.url");
-    EXPECT_FALSE(client.Connect());
-    EXPECT_NE(200, client.error_code());
+    auto response = HTTP(GET("http://999.999.999.999"));
+    EXPECT_NE(200, response.code);
   }
 }
