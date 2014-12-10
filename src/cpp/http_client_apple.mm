6c7e6771a69133c7fb72f3633717b55b4608b022
Dima <dmitry.korolev@gmail.com>
2014-12-09 14:46:55 -1000

    Merging.

--- src/cpp/http_client_apple.mm
+++ src/cpp/http_client_apple.mm
@@ -43,7 +43,7 @@ SOFTWARE.
 
 namespace aloha {
 
-bool HttpClient::Connect() {
+bool HTTPClientPlatformWrapper::RunHTTPRequest() {
   @autoreleasepool {
 
     NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:
@@ -91,7 +91,7 @@ bool HttpClient::Connect() {
       else
         [url_data writeToFile:[NSString stringWithUTF8String:received_file_.c_str()] atomically:YES];
     }
-    return (200 == error_code_);
+    return true;  // TODO(dkorolev) + TODO(deathbaba): Figure out something smarter than return (200 == error_code_);
 
   } // @autoreleasepool
 }
