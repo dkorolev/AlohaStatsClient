a20767d32d6e659d7b30675a550c86df12528dd3
Dima <dmitry.korolev@gmail.com>
2014-12-13 03:13:20 -1000

    Indent.

--- src/bricks/net/api/impl/android.h
+++ src/bricks/net/api/impl/android.h
@@ -170,131 +170,131 @@ class HTTPClientPlatformWrapper {
   // @returns true only if server answered with HTTP 200 OK
   // @note Implementations should transparently support all needed HTTP redirects
   bool Go() {
-  // Attaching multiple times from the same thread is a no-op, which only gets good env for us.
-  JNIEnv* env;
-  if (JNI_OK != ::GetJVM()->AttachCurrentThread(PLATFORM_SPECIFIC_CAST & env, nullptr)) {
-    // TODO(AlexZ): throw some critical exception.
-    return false;
-  }
-
-  // TODO(AlexZ): May need to refactor if this method will be agressively used from the same thread.
-  const auto detachThreadOnScopeExit = bricks::MakeScopeGuard([] { ::GetJVM()->DetachCurrentThread(); });
-
-  // Convenience lambda.
-  const auto deleteLocalRef = [&env](jobject o) { env->DeleteLocalRef(o); };
-
-  // Create and fill request params.
-  const auto jniUrl = MakePointerScopeGuard(env->NewStringUTF(url_requested_.c_str()), deleteLocalRef);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-  const auto httpParamsObject = MakePointerScopeGuard(
-      env->NewObject(g_httpParamsClass, g_httpParamsConstructor, jniUrl.get()), deleteLocalRef);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-  // Cache it on the first call.
-  const static jfieldID dataField = env->GetFieldID(g_httpParamsClass, "data", "[B");
-  if (!post_body_.empty()) {
-    const auto jniPostData = MakePointerScopeGuard(env->NewByteArray(post_body_.size()), deleteLocalRef);
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetByteArrayRegion(
-        jniPostData.get(), 0, post_body_.size(), reinterpret_cast<const jbyte*>(post_body_.data()));
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetObjectField(httpParamsObject.get(), dataField, jniPostData.get());
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  }
-
-  const static jfieldID contentTypeField =
-      env->GetFieldID(g_httpParamsClass, "contentType", "Ljava/lang/String;");
-  if (!content_type_.empty()) {
-    const auto jniContentType = MakePointerScopeGuard(env->NewStringUTF(content_type_.c_str()), deleteLocalRef);
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetObjectField(httpParamsObject.get(), contentTypeField, jniContentType.get());
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  }
-
-  if (!user_agent_.empty()) {
-    const static jfieldID userAgentField =
-        env->GetFieldID(g_httpParamsClass, "userAgent", "Ljava/lang/String;");
-
-    const auto jniUserAgent = MakePointerScopeGuard(env->NewStringUTF(user_agent_.c_str()), deleteLocalRef);
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetObjectField(httpParamsObject.get(), userAgentField, jniUserAgent.get());
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  }
-
-  if (!post_file_.empty()) {
-    const static jfieldID inputFilePathField =
-        env->GetFieldID(g_httpParamsClass, "inputFilePath", "Ljava/lang/String;");
-
-    const auto jniInputFilePath = MakePointerScopeGuard(env->NewStringUTF(post_file_.c_str()), deleteLocalRef);
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetObjectField(httpParamsObject.get(), inputFilePathField, jniInputFilePath.get());
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  }
-
-  if (!received_file_.empty()) {
-    const static jfieldID outputFilePathField =
-        env->GetFieldID(g_httpParamsClass, "outputFilePath", "Ljava/lang/String;");
-
-    const auto jniOutputFilePath =
-        MakePointerScopeGuard(env->NewStringUTF(received_file_.c_str()), deleteLocalRef);
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-    env->SetObjectField(httpParamsObject.get(), outputFilePathField, jniOutputFilePath.get());
-    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  }
-
-  // DO ALL MAGIC!
-  // Current Java implementation simply reuses input params instance, so we don't need to
-  // DeleteLocalRef(response).
-  const jobject response =
-      env->CallStaticObjectMethod(g_httpTransportClass, g_httpTransportClass_run, httpParamsObject.get());
-  if (env->ExceptionCheck()) {
-    env->ExceptionDescribe();
-    // TODO(AlexZ): think about rethrowing corresponding C++ exceptions.
-    env->ExceptionClear();
-    return false;
-  }
-
-  const static jfieldID httpResponseCodeField = env->GetFieldID(g_httpParamsClass, "httpResponseCode", "I");
-  error_code_ = env->GetIntField(response, httpResponseCodeField);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-
-  const static jfieldID receivedUrlField =
-      env->GetFieldID(g_httpParamsClass, "receivedUrl", "Ljava/lang/String;");
-  const auto jniReceivedUrl = MakePointerScopeGuard(
-      static_cast<jstring>(env->GetObjectField(response, receivedUrlField)), deleteLocalRef);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  if (jniReceivedUrl) {
-    url_received_ = std::move(ToStdString(env, jniReceivedUrl.get()));
-  }
-
-  // contentTypeField is already cached above.
-  const auto jniContentType = MakePointerScopeGuard(
-      static_cast<jstring>(env->GetObjectField(response, contentTypeField)), deleteLocalRef);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  if (jniContentType) {
-    content_type_ = std::move(ToStdString(env, jniContentType.get()));
-  }
-
-  // dataField is already cached above.
-  const auto jniData =
-      MakePointerScopeGuard(static_cast<jbyteArray>(env->GetObjectField(response, dataField)), deleteLocalRef);
-  CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
-  if (jniData) {
-    jbyte* buffer = env->GetByteArrayElements(jniData.get(), nullptr);
-    if (buffer) {
-      server_response_.assign(reinterpret_cast<const char*>(buffer), env->GetArrayLength(jniData.get()));
-      env->ReleaseByteArrayElements(jniData.get(), buffer, JNI_ABORT);
+    // Attaching multiple times from the same thread is a no-op, which only gets good env for us.
+    JNIEnv* env;
+    if (JNI_OK != ::GetJVM()->AttachCurrentThread(PLATFORM_SPECIFIC_CAST & env, nullptr)) {
+      // TODO(AlexZ): throw some critical exception.
+      return false;
     }
+
+    // TODO(AlexZ): May need to refactor if this method will be agressively used from the same thread.
+    const auto detachThreadOnScopeExit = bricks::MakeScopeGuard([] { ::GetJVM()->DetachCurrentThread(); });
+
+    // Convenience lambda.
+    const auto deleteLocalRef = [&env](jobject o) { env->DeleteLocalRef(o); };
+
+    // Create and fill request params.
+    const auto jniUrl = MakePointerScopeGuard(env->NewStringUTF(url_requested_.c_str()), deleteLocalRef);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+    const auto httpParamsObject = MakePointerScopeGuard(
+        env->NewObject(g_httpParamsClass, g_httpParamsConstructor, jniUrl.get()), deleteLocalRef);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+    // Cache it on the first call.
+    const static jfieldID dataField = env->GetFieldID(g_httpParamsClass, "data", "[B");
+    if (!post_body_.empty()) {
+      const auto jniPostData = MakePointerScopeGuard(env->NewByteArray(post_body_.size()), deleteLocalRef);
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetByteArrayRegion(
+          jniPostData.get(), 0, post_body_.size(), reinterpret_cast<const jbyte*>(post_body_.data()));
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetObjectField(httpParamsObject.get(), dataField, jniPostData.get());
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    }
+
+    const static jfieldID contentTypeField =
+        env->GetFieldID(g_httpParamsClass, "contentType", "Ljava/lang/String;");
+    if (!content_type_.empty()) {
+      const auto jniContentType = MakePointerScopeGuard(env->NewStringUTF(content_type_.c_str()), deleteLocalRef);
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetObjectField(httpParamsObject.get(), contentTypeField, jniContentType.get());
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    }
+
+    if (!user_agent_.empty()) {
+      const static jfieldID userAgentField =
+          env->GetFieldID(g_httpParamsClass, "userAgent", "Ljava/lang/String;");
+
+      const auto jniUserAgent = MakePointerScopeGuard(env->NewStringUTF(user_agent_.c_str()), deleteLocalRef);
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetObjectField(httpParamsObject.get(), userAgentField, jniUserAgent.get());
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    }
+
+    if (!post_file_.empty()) {
+      const static jfieldID inputFilePathField =
+          env->GetFieldID(g_httpParamsClass, "inputFilePath", "Ljava/lang/String;");
+
+      const auto jniInputFilePath = MakePointerScopeGuard(env->NewStringUTF(post_file_.c_str()), deleteLocalRef);
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetObjectField(httpParamsObject.get(), inputFilePathField, jniInputFilePath.get());
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    }
+
+    if (!received_file_.empty()) {
+      const static jfieldID outputFilePathField =
+          env->GetFieldID(g_httpParamsClass, "outputFilePath", "Ljava/lang/String;");
+
+      const auto jniOutputFilePath =
+          MakePointerScopeGuard(env->NewStringUTF(received_file_.c_str()), deleteLocalRef);
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+      env->SetObjectField(httpParamsObject.get(), outputFilePathField, jniOutputFilePath.get());
+      CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    }
+
+    // DO ALL MAGIC!
+    // Current Java implementation simply reuses input params instance, so we don't need to
+    // DeleteLocalRef(response).
+    const jobject response =
+        env->CallStaticObjectMethod(g_httpTransportClass, g_httpTransportClass_run, httpParamsObject.get());
+    if (env->ExceptionCheck()) {
+      env->ExceptionDescribe();
+      // TODO(AlexZ): think about rethrowing corresponding C++ exceptions.
+      env->ExceptionClear();
+      return false;
+    }
+
+    const static jfieldID httpResponseCodeField = env->GetFieldID(g_httpParamsClass, "httpResponseCode", "I");
+    error_code_ = env->GetIntField(response, httpResponseCodeField);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+
+    const static jfieldID receivedUrlField =
+        env->GetFieldID(g_httpParamsClass, "receivedUrl", "Ljava/lang/String;");
+    const auto jniReceivedUrl = MakePointerScopeGuard(
+        static_cast<jstring>(env->GetObjectField(response, receivedUrlField)), deleteLocalRef);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    if (jniReceivedUrl) {
+      url_received_ = std::move(ToStdString(env, jniReceivedUrl.get()));
+    }
+
+    // contentTypeField is already cached above.
+    const auto jniContentType = MakePointerScopeGuard(
+        static_cast<jstring>(env->GetObjectField(response, contentTypeField)), deleteLocalRef);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    if (jniContentType) {
+      content_type_ = std::move(ToStdString(env, jniContentType.get()));
+    }
+
+    // dataField is already cached above.
+    const auto jniData =
+        MakePointerScopeGuard(static_cast<jbyteArray>(env->GetObjectField(response, dataField)), deleteLocalRef);
+    CLEAR_AND_RETURN_FALSE_ON_EXCEPTION
+    if (jniData) {
+      jbyte* buffer = env->GetByteArrayElements(jniData.get(), nullptr);
+      if (buffer) {
+        server_response_.assign(reinterpret_cast<const char*>(buffer), env->GetArrayLength(jniData.get()));
+        env->ReleaseByteArrayElements(jniData.get(), buffer, JNI_ABORT);
+      }
+    }
+    return true;
   }
-  return true;
-}
 
   std::string const& url_requested() const {
     return url_requested_;
